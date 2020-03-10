7237a7610a0d0aa86d0c7d3c34078711d877543e
OPelham <oliverpelham@gmail.com>
2020-03-10 21:11:27 +1300

    Improved character movement script

---
+++ Assets/CharacterMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    CharacterController characterController;
+
+    public float speed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 15.0f;
+
+    private Vector3 moveDirection = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (characterController.isGrounded)
+        {
+            float horizontalMove = Input.GetAxis("Horizontal");
+            float verticalMove = Input.GetAxis("Vertical");
+
+            moveDirection = transform.right * horizontalMove + transform.forward * verticalMove;
+            moveDirection *= speed;
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        characterController.Move(moveDirection* Time.deltaTime);
+    }
+}
